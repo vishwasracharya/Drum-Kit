482e04411514a78fa0e58d99567e6b48782af7ca
Vishwas Acharya <vishwasracharya@gmail.com>
2021-12-15 17:38:49 +0530

    Day 1 on Drum Kit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+for(let i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("Hello")
+    });
+}
