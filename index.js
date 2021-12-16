07af1281bf6db1d6bc70b7a7ead84321bde742a8
Vishwas Acharya <vishwasracharya@gmail.com>
2021-12-16 16:17:21 +0530

    Day 2 on Drum Kit - Project Complete

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
-
-for(let i=0; i<document.querySelectorAll(".drum").length; i++){
+let numOfDrumBtn = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numOfDrumBtn; i++) {
+    
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("Hello")
+        let btnInnerHTML = this.innerHTML;
+        makeSound(btnInnerHTML);
+        btnAnimation(btnInnerHTML);
+
     });
+}
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    btnAnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+            
+        default: console.log("Plese enter valid data");
+    }
+}
+
+function btnAnimation(currentKey){
+    let activeBtn = document.querySelector("." + currentKey);
+    activeBtn.classList.add("pressed");
+
+    setTimeout(function(){
+        activeBtn.classList.remove("pressed");
+    }, 100);
 }
